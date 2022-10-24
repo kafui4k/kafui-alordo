9f0ff7014c61c59c557b3b5793c20e08202f8e7f
Kafui Alordo <kalordo7@gmail.com>
2022-10-24 01:28:58 +0000

    add a footer component, about page and update the hompage

---
+++ components/footer-component.tsx
@@ -0,0 +1,17 @@
+import styles from "../styles/Home.module.css";
+
+const FooterComponent = () => {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/kafui4k"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        &copy; 2022 kafui
+      </a>
+    </footer>
+  );
+};
+
+export default FooterComponent;
